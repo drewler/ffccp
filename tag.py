97e0b741884272f0ac789da873131dfc0c58d816
Drewler <drewler@gmail.com>
2014-10-14 18:01:09 +0200

    Removed class attributes
    Fixed data not being properly initiated
    Fixed binary data not being properly assigned

--- tag.py
+++ tag.py
@@ -3,12 +3,12 @@ import sys
 import re
 
 class Tag:
-    offset = 0
-    type = ""
-    length = 0
+    offset = None
+    type = None
+    length = None
     mystery_data = None
-    subtags = []
-    binary_data = b""
+    subtags = None
+    binary_data = None
     parsed_data = None
     def __init__(self, stream_data):
         self.offset = stream_data.tell()
@@ -16,15 +16,16 @@ class Tag:
         self.length = struct.unpack(">I", stream_data.read(4))[0]
         self.mystery_data = stream_data.read(8)
         self.subtags = []
-        binary_data = b""
-        parsed_data = None
+        self.binary_data = b""
+        self.parsed_data = None
         while (stream_data.tell() - self.offset) <= self.length:
             first4 = stream_data.read(4)
             if re.match(b"[A-Z| ]{4}", first4):
                 stream_data.seek(stream_data.tell() - 4)
                 self.subtags.append(Tag(stream_data))
             else:
-                self.binary_data.join([first4, stream_data.read(12)])
+                self.binary_data = b"".join([self.binary_data, first4, stream_data.read(12)])
+        self.align(stream_data, 16)
     def align(self, stream_data, to):
         stream_data.seek(int(stream_data.tell() + to - 1 - (stream_data.tell() - 1) % to))
     def __repr__(self):
@@ -33,6 +34,8 @@ class Tag:
         else:
             return "\n%s - %s" % (self.type, str(self.subtags))    
 
-fh = open(sys.argv[1], "rb");
-tagroot = Tag(fh)
-print tagroot
+# If called as script, read tags & print them
+if __name__ == "__main__":
+    fh = open(sys.argv[1], "rb")
+    tagroot = Tag(fh)
+    print tagroot
