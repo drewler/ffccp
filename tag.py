3fd296c6457fc5085bd268ffa027a93ba7995323
Drewler <drewler@gmail.com>
2014-10-15 23:27:53 +0200

    Fixed reading wrong binary data for tags

--- tag.py
+++ tag.py
@@ -18,13 +18,14 @@ class Tag:
         self.subtags = []
         self.binary_data = b""
         self.parsed_data = None
-        while (stream_data.tell() - self.offset) <= self.length:
+        while (stream_data.tell() - (self.offset + 16)) < self.length:
             first4 = stream_data.read(4)
+            stream_data.seek(stream_data.tell() - 4)
             if re.match(b"[A-Z| ]{4}", first4):
-                stream_data.seek(stream_data.tell() - 4)
                 self.subtags.append(Tag(stream_data))
             else:
-                self.binary_data = b"".join([self.binary_data, first4, stream_data.read(12)])
+                readlen = min(16, (self.offset + 16 + self.length) - stream_data.tell())
+                self.binary_data = b"".join([self.binary_data, stream_data.read(readlen)])
         self.align(stream_data, 16)
     def align(self, stream_data, to):
         stream_data.seek(int(stream_data.tell() + to - 1 - (stream_data.tell() - 1) % to))
